846b5c9c0d0259a25722e99277771170dfb570ac
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-20 09:49:06 +0100

    Created routing

--- invoice-app/src/Routes/Routing.tsx
+++ invoice-app/src/Routes/Routing.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "../Pages";
 
 const Routing = () => {
-  return <div>Routes</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default Routing;
