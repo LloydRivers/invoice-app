d5fdd36f16e2659b541f90b3d01027f792f46ce3
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-22 11:03:35 +0100

    Added a max width
    I have done this because when the screen gets massive, it is breaking the Logo icon in the sidenav

--- invoice-app/src/Pages/Home/Home.styled.js
+++ invoice-app/src/Pages/Home/Home.styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const StyledHome = styled.main`
   background-color: ${(props) => props.theme.heroBG};
+  max-width: 1200px;
 
   display: grid;
   grid-template-columns: 1fr 11fr;
